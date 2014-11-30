4d8d24623f60054ee70e6cde840ea995a5629a69
kemiisto <kemiisto@gmail.com>
2014-11-30 18:57:58 +0300

    Converted indentation to spaces.

--- c99/ch01/test_search.c
+++ c99/ch01/test_search.c
@@ -6,15 +6,15 @@ void test_search_linear_search(void);
 
 int main(void)
 {
-	printf("Testing...\n\n");
+    printf("Testing...\n\n");
 
-	printf("search_linear_search()\n");
-	test_search_linear_search();
+    printf("search_linear_search()\n");
+    test_search_linear_search();
 
-	return 0;
+    return 0;
 }
 
 void test_search_linear_search(void)
 {
-	int array[] = { 8, 7, 0, 6, 4, 0, 5, 5, 7, 9 };
+    int array[] = { 8, 7, 0, 6, 4, 0, 5, 5, 7, 9 };
 }
