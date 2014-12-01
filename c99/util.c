5dd3a47c72b86a800ba2d7f31daa42f43d97c7a2
kemiisto <kemiisto@gmail.com>
2014-12-01 22:25:07 +0300

    Few CMake improvements and MSVC support for C99 sampes.

--- c99/util.c
+++ c99/util.c
@@ -6,7 +6,7 @@
 int* util_random_int_array(size_t count, int min_value, int max_value)
 {
     int* array = malloc(count * sizeof(int));
-    srand(time(0));
+    srand((unsigned int) time(0));
     for (size_t i = 0; i < count; ++i) {
         array[i] = rand() % (max_value - min_value) + min_value;
     }
